02326dca8dd21b54bd3f52508e25e5a8ece6c3d7
wenmagi <magiwen@magiwen-PC343.21kunpeng.local>
2014-09-12 10:27:54 +0800

    first

---
+++ src/TestToolDB/UI/WelcomUI.java
@@ -0,0 +1,19 @@
+package TestToolDB.UI;
+
+import com.example.testtooldb.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomUI extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcom_ui);
+	}
+
+	
+	
+}
